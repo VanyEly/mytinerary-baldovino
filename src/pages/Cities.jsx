2a374ee9d52deb8ee19826606e088dd4e9c29a68
Vanina <vani_93_00@hotmail.com>
2023-08-09 21:18:47 -0300

    cities

---
+++ src/pages/Cities.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+function Cities() {
+  return (
+    <>
+    <Layout/>
+    
+    <Layout/>
+    </>
+  )
+}
+
+export default Cities
